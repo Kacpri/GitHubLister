ca3f5e0c2fd2da10e9c3536079277f95d8ebde80
Kacper Piotrowicz <kacpri7@gmail.com>
2021-04-24 19:09:55 +0200

    Add form and route to main page

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, RadioField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
+app.config.update(dict(
+    SECRET_KEY='secret_key',
+    CSRF_ENABLED=True,
+))
+
+
+class GitHubForm(FlaskForm):
+    name = StringField(validators=[DataRequired()],
+                       id='nameInput',
+                       render_kw={'placeholder': 'Podaj nazwę'})
+
+    type = RadioField(choices=[(0, 'Organizacja'), (1, 'Użytkownik')],
+                      default=0,
+                      validators=[DataRequired()],
+                      id='typeInput')
+
+    submit = SubmitField(label="Szukaj")
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    form = GitHubForm(request.form)
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
